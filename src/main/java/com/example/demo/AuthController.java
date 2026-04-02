27c632e40855263d9c82180df76c62bd661062da
Bharathhh30 <vnsb30@gmail.com>
2026-04-02 22:42:44 +0530

    backend23

--- src/main/java/com/example/demo/AuthController.java
+++ src/main/java/com/example/demo/AuthController.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://leave-application-one.vercel.app")
 public class AuthController {
 
     @Autowired
